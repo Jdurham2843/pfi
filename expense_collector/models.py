603c73d397507cd94fef03796c5196f7b1f890b5
John Durham <jdurham@Johns-MacBook-Pro.local>
2017-11-01 22:09:48 -0500

    refactor(master): wrote rough version of test_retrieve_debit_from_collector

--- expense_collector/models.py
+++ expense_collector/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Debit(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    #tags = 
+    description = models.CharField(max_length=400)
--- expense_collector/tests.py
+++ expense_collector/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Debit
 
 # Create your tests here.
 class DebitTests(TestCase):
@@ -6,7 +7,18 @@ class DebitTests(TestCase):
         pass
     
     def test_retrieve_debit_from_collector(self):
-        pass
+        new_debit = Debit.objects.create(name="Eggs", price=0.52, description="eggs have protein")
+        new_debit.save()
+
+        ret_debit = Debit.objects.first()
+
+        self.assertEqual(ret_debit.name, "Eggs")
+        self.assertEqual(format(ret_debit.price, '.2f'), '0.52')
+        #self.assertEqual(ret_debit.tags, ["grocery", "eggs"])
+        self.assertEqual(ret_debit.description, "eggs have protein")
+
+
+
     
     def test_update_debit_to_collector(self):
         pass
