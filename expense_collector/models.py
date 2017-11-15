7e9822bba90ec34a99d759a9a36dbd486bb3f056
John Durham <jdurham@Johns-MacBook-Pro.local>
2017-11-14 20:57:05 -0600

    finalize data models

--- expense_collector/models.py
+++ expense_collector/models.py
@@ -2,10 +2,21 @@
 from django.db import models
 import uuid
 
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
 class TransactionSet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=400)
+    tags = models.ManyToManyField(Tag, related_name="transaction_set")
 
 class Transaction(models.Model):
     DEBIT = 'DB'
@@ -24,6 +35,8 @@ class Transaction(models.Model):
     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=400)
+    transaction_set = models.ForeignKey(TransactionSet, on_delete=models.CASCADE, default=None)
+    tags = models.ManyToManyField(Tag, related_name="transaction")
 
     def __str__(self):
         return '<{type}: {name} ${price} -- \"{description}\">'.format(
@@ -32,16 +45,3 @@ class Transaction(models.Model):
     
     class Meta:
         ordering = ('name', 'type', 'price', 'description',)
-
-
-class Tag(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, unique=True)
-    transactions = models.ManyToManyField(Transaction)
-    transaction_sets = models.ManyToManyField(TransactionSet)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ('name',)
