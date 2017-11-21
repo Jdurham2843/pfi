81e2ee881284798d39f394c4ed146537fb0eec6d
John Durham <jdurham@Johns-MacBook-Pro.local>
2017-11-20 20:35:43 -0600

    refactor(master): extract functions from create_transaction_controller

--- expense_collector/urls.py
+++ expense_collector/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello, name='hello'),
-    url(r'^create-transaction$', views.create_transaction,
-        name='create_transaction'),
+    url(r'^create-transaction$', views.create_transaction_controller,
+        name='create_transaction_controller'),
 ]
--- expense_collector/views.py
+++ expense_collector/views.py
@@ -4,30 +4,35 @@ from django.http import HttpResponse, Http404
 from .models import Transaction, Tag
 
 
-def hello(request):
-    return HttpResponse('Hello world')
+# Create Transaction View + helpers    
+def create_transaction_controller(request):
+    if request.method == 'POST':
+        transaction = create_transaction(request)
+
+        tags_list = request.POST.get('tags').split('|||')
+        
+        if tags_list:
+            add_tags_to_transaction(transaction, tags_list)
+        return HttpResponse('OK')
+    else:
+        raise Http404()
 
 
 def create_transaction(request):
-    if request.method == 'POST':
-        transaction = Transaction.objects.create(
+    transaction = Transaction.objects.create(
             name=request.POST.get('name'),
             amount=Decimal(request.POST.get('amount')),
             type=request.POST.get('type'),
             description=request.POST.get('description'))
-        transaction.save()
+    return transaction
 
-        tags_list = request.POST.get('tags').split('|||')
-        
-        for tag in tags_list:
+
+def add_tags_to_transaction(transaction, tags_list):
+    for tag in tags_list:
             current_tag = Tag.objects.filter(name=tag)
             if current_tag.count() > 0:
                 transaction.tags.add(current_tag)
             else:
                 new_tag = Tag.objects.create(name=tag)
-                new_tag.save()
                 transaction.tags.add(new_tag)
             transaction.save()
-        return HttpResponse('OK')
-    else:
-        raise Http404()
