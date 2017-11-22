e79f199a31054aa2f70b184ce387e77089dfba86
John Durham <jdurham@Johns-MacBook-Pro.local>
2017-11-22 16:00:06 -0600

    refactor(master): pull business logic out of views into service

---
+++ expense_collector/services/transaction_service.py
@@ -0,0 +1,27 @@
+""" Service to retrive transaction related objects """
+from decimal import Decimal
+
+from django.db import transaction
+from ..models import Transaction, Tag
+
+
+@transaction.atomic
+def create_transaction(request):
+    """ Helper function to create Transaction object """
+    transaction = Transaction.objects.create(
+            name=request.POST.get('name'),
+            amount=Decimal(request.POST.get('amount')),
+            type=request.POST.get('type'),
+            description=request.POST.get('description'))
+
+    return transaction
+
+
+def add_tags_to_transaction(transaction, tags_list):
+    for tag in tags_list:
+            current_tag = Tag.objects.filter(name=tag)
+            if current_tag.count() > 0:
+                transaction.tags.add(current_tag)
+            else:
+                new_tag = Tag.objects.create(name=tag)
+                transaction.tags.add(new_tag)
--- expense_collector/views.py
+++ expense_collector/views.py
@@ -1,18 +1,18 @@
-from decimal import Decimal
+""" Views for expense_collector """
 
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-from django.db import IntegrityError, transaction
+from django.db import IntegrityError
 from django.urls import reverse
 
-from .models import Transaction, Tag
+from .services import transaction_service
 
 
 # Create Transaction View + helpers
 def create_transaction_view(request):
     if request.method == 'POST':
         try:
-            transaction = create_transaction(request)
+            transaction = transaction_service.create_transaction(request)
         except IntegrityError:
             raise Http404()
 
@@ -22,31 +22,10 @@ def create_transaction_view(request):
             pass
         else:
             if tags_list:
-                add_tags_to_transaction(transaction, tags_list)
+                transaction_service.add_tags_to_transaction(transaction,
+                                                            tags_list)
         finally:
             return HttpResponseRedirect(reverse('create_transaction_view'))
     elif request.method == 'GET':
         #TODO: build out transaction html page
         return render(request, 'pfi/index.html', {})
-
-
-@transaction.atomic
-def create_transaction(request):
-    """ Helper function to create Transaction object """
-    transaction = Transaction.objects.create(
-            name=request.POST.get('name'),
-            amount=Decimal(request.POST.get('amount')),
-            type=request.POST.get('type'),
-            description=request.POST.get('description'))
-
-    return transaction
-
-
-def add_tags_to_transaction(transaction, tags_list):
-    for tag in tags_list:
-            current_tag = Tag.objects.filter(name=tag)
-            if current_tag.count() > 0:
-                transaction.tags.add(current_tag)
-            else:
-                new_tag = Tag.objects.create(name=tag)
-                transaction.tags.add(new_tag)
