b3ddbcff1c10568363dbf54904bc1a67334bc38e
John Durham <jdurham@Johns-MacBook-Pro.local>
2017-11-22 17:00:41 -0600

    refactor(master): change id to uuid

--- expense_collector/models.py
+++ expense_collector/models.py
@@ -4,7 +4,7 @@ import uuid
 
 
 class Tag(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Tag(models.Model):
 
 
 class TransactionSet(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=400)
     tags = models.ManyToManyField(Tag, related_name="transaction_set")
@@ -30,7 +30,7 @@ class Transaction(models.Model):
         (CREDIT, 'Credit'),
     )
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     type = models.CharField(
         max_length=2,
